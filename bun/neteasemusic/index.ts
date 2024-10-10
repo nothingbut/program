a6e08cbc58b0d5d34a58e9a715a6307e82030eba
nothingbut <shi.chang@163.com>
2024-10-10 08:52:47 +0800

    cleanup

--- bun/neteasemusic/index.ts
+++ bun/neteasemusic/index.ts
@@ -1,9 +1,9 @@
-const NodeID3 = require('node-id3')
-const NeteaseMusic = require('netease-cloud-music')
+import * as NodeID3 from 'node-id3'
+import * as NeteaseMusic from 'netease-cloud-music'
 
-var tags = NodeID3.read('/Users/shichang/Music/take/两年一度1819/梦在黎明破晓时 - 盘尼西林.mp3')
+let tags = NodeID3.read('/Users/shichang/Music/take/两年一度1819/梦在黎明破晓时 - 盘尼西林.mp3')
 NeteaseMusic.login('13701070330', 'Zte1Desk!@', true)
-NeteaseMusic.search(tags.title, 0, 100, 1).then((data) => {
+NeteaseMusic.search(tags.title, 0, 100, 1).then((data: any) => {
     console.log(data)
 })
 
--- bun/txtsplitter/index.ts
+++ bun/txtsplitter/index.ts
@@ -1,20 +1,21 @@
-const sourcefile: string = 'data/sample.txt';
-const pattern = /(正文){0,1}(第)([\u4e00-\u9fa5a-zA-Z0-9]{1,7})[卷集章部篇节回]((?! {4}).){0,50}\r?\n/g;
-var iconv = require('iconv-lite');
-var fs = require('fs');
+import iconv from 'iconv-lite'
+import fs from 'fs';
 
-const content = fs.readFileSync(sourcefile);
+const sourcefile: string = 'data/sample.txt';
+const pattern: RegExp = /(正文){0,1}(第)([\u4e00-\u9fa5a-zA-Z0-9]{1,7})[卷集章部篇节回]((?! {4}).){0,50}\r?\n/g;
+const content: Buffer = fs.readFileSync(sourcefile);
+
 splitText(content);
 
-function splitText(str) {
+function splitText(str: Buffer) {
 
-    let chaptList = [];
+    let chaptList: string[] = [];
     const pieces = iconv.decode(str, 'GB2312').match(pattern);
     pieces && addChapter(chaptList, ...pieces);
     console.log(chaptList);
     return chaptList;
 }
 
-function addChapter(list, piece) {
+function addChapter(list: string[], piece: string) {
     list.push(piece);
 }
